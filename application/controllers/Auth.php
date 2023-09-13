f1e6f986004cf17fc1b58e7cb532a7fc301a4dec
Horia Manea <horiamanea.m@gmail.com>
2023-09-14 01:05:08 +0300

    first commit

---
+++ application/controllers/Auth.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+
+	public function index()
+	{
+		$this->load->view('Auth/login');
+	}
+
+	public function register()
+	{
+		$this->load->view('Auth/register');
+	}
+
+	public function registration_form()
+	{
+		$this->auth_model->register_user();
+	}
+
+	public function login_form()
+	{
+		$this->auth_model->login_user();
+	}
+
+	public function main()
+	{
+		$this->load->view('Auth/index');
+	}
+}
